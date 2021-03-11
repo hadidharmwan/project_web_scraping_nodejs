a0815f5d3a8012809f27452554d8361a758af3e5
Kuja Dconnor <rubynapi321@gmail.com>
2021-03-11 19:19:21 +0700

    add function getLastestHeadline, and get data, show

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ function getFromCNN(resp) {
     fetch(cnn).then(res => res.text()).then((html) => { resp(html);})
 };
 
+function getLastestHeadline(data) {
+    const $ = cheerio.load(data);
+
+    let Titles = [];
+    $('ul').children('li').each(function(i, el){
+        Titles.push({title : $(el).text(), link : $(el).children('a').attr('href')})
+    })
+    return Titles[0];   
+}
+
+getFromCNN((data) => {
+    console.log(getLastestHeadline(data))
+})
