35b94c490d4c35b3a3149fe45a48f54b143b9ec2
Kuja Dconnor <rubynapi321@gmail.com>
2021-03-11 23:36:29 +0700

    add function sendmail

--- mailSender.js
+++ mailSender.js
@@ -1,2 +1,36 @@
 const nodemailer = require('node-mailer');
 const dotenv = require('dotenv');
+
+dotenv.config()
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+var mailOptions = {
+    from: process.env.USER,
+    to: 'hadidharmawan32@gmail.com',
+    subject: '',
+    text: ''
+};
+
+function sendMail(subject, text, link, url) {
+    mailOptions.subject = subject;
+    mailOptions.html = "<a href="+url+link+ ">" + text + "</a>";
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+}
+
+module.exports = sendMail;
