756613aee5392c63399c0f701c4054e15ca79705
Kuja Dconnor <rubynapi321@gmail.com>
2021-03-11 18:48:08 +0700

    add cnn, string and function to get url

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const cheerio = require('cheerio');
 const nodemailer = require('node-mailer');
 const fetch = require('node-fetch');
+
+const cnn = 'https://lite.cnn.com/en';
+const string = 'Trump';
+
+function getFromCNN(resp) {
+    fetch(cnn).then(res => res.text()).then((html) => { resp(html);})
+};
+
